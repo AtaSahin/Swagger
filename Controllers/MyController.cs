21c520e0bf5035658132dbfc56abc1ecdd301740
Ata Sahin <atasahin05@gmail.com>
2023-12-10 17:44:20 +0300

    swagger

---
+++ Controllers/MyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Swagger.Web.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class MyController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> GetItems()
+        {
+            return new List<string> { "Item 1", "Item 2", "Item 3" };
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Swagger.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
